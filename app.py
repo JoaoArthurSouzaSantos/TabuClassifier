722bff37ccb1d9c5d33b2cc5d81043380af74746
yagomaia <yagohenriquev123@gmail.com>
2024-08-19 15:56:06 -0300

    Estrutura do projeto - Separação Frontend e Backend

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask
-import os
-import numpy as np
-from Frontend.routes import home
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from utils import algorithms
+# from frontend.routes import home
 
-app = Flask(__name__)
-app.register_blueprint(home)
+app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
+# app.register_blueprint(home)
 
-@app.route('/', methods=['GET', 'POST'])
+selected_algorithms = []
+
+#criar um arquivo só pra rotas
+@app.route('/')
 def index():
-    return "Home"
+    return render_template('index.html', algorithms=algorithms, selected_algorithms=selected_algorithms)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/add_algorithm', methods=['POST'])
+def add_algorithm():
+    algorithm_name = request.json['algorithm']
+    selected_algorithm = next(algo for algo in algorithms if algo['name'] == algorithm_name)
+    selected_algorithms.append(selected_algorithm)
+    rendered_html = render_template('algorithm_item.html', algorithm=selected_algorithm)
+    return jsonify({"html": rendered_html})
+
+@app.route('/remove_algorithm', methods=['POST'])
+def remove_algorithm():
+    algorithm_name = request.json['algorithm']
+    global selected_algorithms
+    selected_algorithms = [algo for algo in selected_algorithms if algo['name'] != algorithm_name]
+    return jsonify({"success": True})
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Aqui você pode processar os dados de todos os algoritmos selecionados
+    # Por exemplo, renderizar uma página de resumo ou salvar em um banco de dados
+    return redirect(url_for('index'))
